245d8e2360d2b486dcc2ca5bb12764df2897c512
Inworker <31877506+Inworker@users.noreply.github.com>
2024-07-17 23:47:22 +0300

    Задание №4: Создать контакт с  вспомогательными методами и классами

---
+++ webaddressbook-web-tests/webaddressbook-web-tests/ContactData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class ContactData
+    {
+        private string firstname;
+        private string lastname;
+        public ContactData(string firstname, string lastname)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+        
+        public string Firstname
+        {
+            get { return this.firstname; }
+            set { this.firstname = value; }
+        }
+
+        public string LastName
+        {
+            get { return this.lastname; }
+            set { this.lastname = value; }
+        }
+    
+    }
+}
