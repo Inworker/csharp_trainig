260e7affccb94480baf2e7381422ce607f626062
Inworker <31877506+Inworker@users.noreply.github.com>
2024-08-12 22:51:17 +0300

    Дописал методы для ContactHelper

--- webaddressbook-web-tests/webaddressbook-web-tests/appmanager/ContactHelper.cs
+++ webaddressbook-web-tests/webaddressbook-web-tests/appmanager/ContactHelper.cs
@@ -11,23 +11,77 @@ namespace webaddressbook_web_tests
     public class ContactHelper : HelperBase
     {
         
-        public ContactHelper(IWebDriver driver) : base(driver)
+        public ContactHelper(ApplicationManager manager) : base(manager)
         {
 
         }
 
         public void CreateNewContact(WebAddressbookTests.ContactData contact)
         {
-            driver.FindElement(By.LinkText("add new")).Click();
-            driver.FindElement(By.Name("firstname")).Click();
-            driver.FindElement(By.Name("firstname")).Clear();
-            driver.FindElement(By.Name("firstname")).SendKeys(contact.Firstname);
-            driver.FindElement(By.Name("lastname")).Click();
-            driver.FindElement(By.Name("lastname")).Clear();
-            driver.FindElement(By.Name("lastname")).SendKeys(contact.LastName);
-            driver.FindElement(By.Name("submit")).Click();
+            AddContact();
+            ClickFrameFirstName();
+            ClearFirstName();
+            EnterFirstName(contact);
+            ClickFrameLastName();
+            ClearFrameLastName();
+            EnterLastName(contact);
+            ClickButtonSubmit();
         }
 
+        public ContactHelper ClickButtonSubmit()
+        {
+            driver.FindElement(By.Name("submit")).Click();
+            return this;
 
+        }
+
+        public ContactHelper EnterLastName(ContactData contact)
+        {
+            driver.FindElement(By.Name("lastname")).SendKeys(contact.LastName);
+            return this;
+
+        }
+
+        public ContactHelper ClearFrameLastName()
+        {
+            driver.FindElement(By.Name("lastname")).Clear();
+            return this;
+
+        }
+
+        public ContactHelper ClickFrameLastName()
+        {
+            driver.FindElement(By.Name("lastname")).Click();
+            return this;
+
+        }
+
+        public ContactHelper EnterFirstName(ContactData contact)
+        {
+            driver.FindElement(By.Name("firstname")).SendKeys(contact.Firstname);
+            return this;
+
+        }
+
+        public ContactHelper ClearFirstName()
+        {
+            driver.FindElement(By.Name("firstname")).Clear();
+            return this;
+
+        }
+
+        public ContactHelper ClickFrameFirstName()
+        {
+            driver.FindElement(By.Name("firstname")).Click();
+            return this;
+
+        }
+
+        public ContactHelper AddContact()
+        {
+            driver.FindElement(By.LinkText("add new")).Click();
+            return this;
+
+        }
     }
 }
